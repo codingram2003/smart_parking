034659d0a88471cb8ca06afde970fe7ba8c19abd
codingram2003 <shriramrayakar@gmail.com>
2024-06-07 05:56:03 +0000

    first

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_mysqldb import MySQL
+app = Flask(__name__)
+
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'slots'
+
+mysql = MySQL(app)
+
+slots =['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3', 'D1', 'D2', 'D3']
+@app.route('/')
+def hello():
+    curr= mysql.connection.cursor()
+    booked = curr.execute("SELECT * FROM slots")
+    booked = list(curr.fetchall())
+    for i in range(len(booked)):
+        booked[i] = booked[i][1]
+    return render_template('landing.html', slots=slots, booked = booked, l= len(booked))
+
+@app.route('/update', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        slot = details['fname']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO slots(SlNo, slot) VALUES (10, %s)", [firstNam])
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('index.html')
+
+@app.route('/updation', methods=['POST'])
+def update():
+        details = request.form
+        slot = details['slot']
+        length = details['length']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO slots(SlNo, slot) VALUES (%s, %s)", [length, slot])
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/')
+    
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
